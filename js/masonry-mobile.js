999c7aaea53bb6cda2ef9790cffa199feedaf958
xverty67mango <viktorbatuk801@gmail.com>
2026-07-19 09:17:42 +0300

    :/
    надеюсь последний апдейт

--- js/masonry-mobile.js
+++ js/masonry-mobile.js
@@ -4,7 +4,12 @@
         if (grid) {
             const msnry = new Masonry(grid, {
                 itemSelector: '.card',
-                columnWidth: '50%',
+                columnWidth: function() {
+                    // Вычисляем ширину колонки: 50% от ширины контейнера минус половина отступа
+                    const containerWidth = grid.offsetWidth;
+                    const gutter = 12; // Расстояние между колонками
+                    return (containerWidth - gutter) / 2;
+                },
                 gutter: 12
             });
         }
